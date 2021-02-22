26b6d05b738a56a9f5252662661d12fd8e78bf16
JordanMendes <jordan_mendes@hotmail.fr>
2021-02-22 15:40:53 +0100

    premiere version de site

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>PGT</title>
+</head>
+<body>
+<header>
+<ul class="nav row text-center">
+  <li class="nav-item col-md-3">
+    <a class="nav-link active" aria-current="page" href="#">Mon project Git</a>
+  </li>
+  <li class="nav-item col-md-3">
+    <a class="nav-link" href="#">News</a>
+  </li>
+  <li class="nav-item col-md-3">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+  <li class="nav-item col-md-3">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Autre</a>
+  </li>
+</ul>
+</header>
+    <div class="container">
+    
+        <h1 class="display-4 text-center my-5">Mon Project Git</h1>
+
+        <h2>Petit test de nouvelle branche</h2>
+
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="img/slide1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/slide2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/slide3.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+      
+    
+    </div>
+    <footer class="bg-light text-center py-3">
+        &copy; 2021 - JM - Pas touche c'est mon site !
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</body>
+</html>
